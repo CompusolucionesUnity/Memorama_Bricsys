c0a2eba232af179aa569d22de4110c0ac2c8c0bb
CompusolucionesUnity <omendozacs@gmail.com>
2026-05-26 10:18:13 -0600

    Memorama Bricsys

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "CompuSoluciones-Memorama Bricsys-1.0";
+const contentToCache = [
+    "Build/Builds_B.loader.js",
+    "Build/Builds_B.framework.js.unityweb",
+    "Build/Builds_B.data.unityweb",
+    "Build/Builds_B.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
